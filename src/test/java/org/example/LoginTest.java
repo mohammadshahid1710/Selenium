abaf7227ef7118f448ef38cbb67ef22ed759f64c
Mohammad <mohammadshahid1710@gmail.com>
2024-04-10 08:09:39 +0530

    Fully completed login automation feature

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -1,7 +1,11 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginTest extends Base {
 
@@ -12,14 +16,32 @@ public class LoginTest extends Base {
         driver.findElement(By.id("inputUsername")).sendKeys("rahul");
         driver.findElement(By.name("inputPassword")).sendKeys("hello123");
         driver.findElement(By.className("signInBtn")).click();
-        System.out.println(driver.findElement(By.cssSelector ("p.error")).getText());
+
         driver.findElement(By.linkText("Forgot your password?")).click();
         CommonUtils.waitTime(1);
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("rahul");
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("rahul@gmail.com");
         driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("9876543210");
         driver.findElement(By.xpath("//button[text()='Reset Login']")).click();
-        System.out.println(driver.findElement(By.xpath("//form/p[1]")).getText());
+
+        String text = driver.findElement(By.xpath("//form/p[1]")).getText();
+        String patternString = "'(.*?)'";
+
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(text);
+        var password = "";
+        while (matcher.find()) {
+              password = matcher.group(1);
+        }
+       driver.findElement(By.xpath("//button[contains(@class,'login')]")).click();
+        CommonUtils.waitTime(2);
+
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.name("inputPassword")).sendKeys(password);
+        driver.findElement(By.id("chkboxOne")).click();
+        boolean checkBoxClick = driver.findElement(By.id("chkboxOne")).isEnabled();
+        driver.findElement(By.className("signInBtn")).click();
+        Assert.assertTrue(checkBoxClick);
 
 
 
